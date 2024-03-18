3be02b6d52fe61132adb1fad34c590ff8f9fcdb7
dsomorov <dsomorov@gmail.com>
2024-03-18 15:36:37 -0500

    add GameDto and GET routes

---
+++ DTOs/GameDto.cs
@@ -0,0 +1,12 @@
+﻿namespace asp_game_store.DTOs;
+
+public record class GameDto(
+  int Id,
+  string Name,
+  string Genre,
+  decimal Price,
+  DateOnly ReleaseDate
+)
+{
+
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
+using asp_game_store.DTOs;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+List<GameDto> games = [
+  new (1, "Street Fighter II", "Fighting", 19.99M, new DateOnly(1992, 7, 15)),
+  new (2, "Final Fantasy XIV", "Roleplaying", 59.99M, new DateOnly(2010, 9, 30)),
+  new (1, "FIFA 23", "Sports", 69.99M, new DateOnly(2022, 9, 27)),
+];
+
+// GET /games
+app.MapGet("games", () => games);
+
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
