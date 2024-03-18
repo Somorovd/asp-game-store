af71d99d898c0407957b7f06ef01b0c418e5d7ab
dsomorov <dsomorov@gmail.com>
2024-03-18 16:15:48 -0500

    add validation and nuget package

--- GameStore.Api/DTOs/CreateGameDto.cs
+++ GameStore.Api/DTOs/CreateGameDto.cs
@@ -1,9 +1,18 @@
-﻿namespace GameStore.Api.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Api.DTOs;
 
 public record class CreateGameDto(
-  int Id,
+  [Required]
+  [StringLength(50)]
   string Name,
+
+  [Required]
+  [StringLength(20)]
   string Genre,
+
+  [Range(1, 100)]
   decimal Price,
+
   DateOnly ReleaseDate
 );
--- GameStore.Api/DTOs/GamesEndpoints.cs
+++ GameStore.Api/DTOs/GamesEndpoints.cs
@@ -12,13 +12,16 @@ public static class GamesEndpoints
     new (3, "FIFA 23", "Sports", 69.99M, new DateOnly(2022, 9, 27)),
   ];
 
-  public static WebApplication MapGamesEndpoints(this WebApplication app)
+  public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
   {
+    var group = app.MapGroup("games")
+                  .WithParameterValidation();
+
     // GET /games
-    app.MapGet("games", () => games);
+    group.MapGet("/", () => games);
 
     // GET /games/1
-    app.MapGet("games/{id}", (int id) =>
+    group.MapGet("/{id}", (int id) =>
     {
       GameDto? game = games.Find(game => game.Id == id);
 
@@ -27,7 +30,7 @@ public static class GamesEndpoints
     }).WithName(GetGameEndpoint);
 
     // POST /games
-    app.MapPost("games", (CreateGameDto newGame) =>
+    group.MapPost("/", (CreateGameDto newGame) =>
     {
       GameDto game = new GameDto(
         games.Count + 1,
@@ -43,7 +46,7 @@ public static class GamesEndpoints
     });
 
     // PUT /games/1
-    app.MapPut("games/{id}", (int id, UpdateGameDto updatedGame) =>
+    group.MapPut("/{id}", (int id, UpdateGameDto updatedGame) =>
     {
       var index = games.FindIndex(game => game.Id == id);
 
@@ -64,12 +67,12 @@ public static class GamesEndpoints
     });
 
     //DELETE /games/1
-    app.MapDelete("games/{id}", (int id) =>
+    group.MapDelete("/{id}", (int id) =>
     {
       games.RemoveAll(game => game.Id == id);
       return Results.NoContent();
     });
 
-    return app;
+    return group;
   }
 }
