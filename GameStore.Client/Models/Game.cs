74ae8589eeb5ff2fbeaddca8ed2d35d690c5467c
dsomorov <dsomorov@gmail.com>
2024-03-20 14:34:42 -0500

    add form validation

--- GameStore.Client/Models/Game.cs
+++ GameStore.Client/Models/Game.cs
@@ -1,11 +1,22 @@
-﻿namespace GameStore.Client.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Client.Models;
 
 public record Game
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required]
+    [StringLength(20)]
     public required string Genre { get; set; }
+
+    [Range(1, 100)]
     public decimal Price { get; set; }
+
     public DateOnly ReleaseDate { get; set; }
 
 }
